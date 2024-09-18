0ec2165f38fa811b45a85faf27a2f8c1a8689c0d
arrival-spring <77166354+arrival-spring@users.noreply.github.com>
2024-09-18 10:26:09 +0200

    Find more features in YextAnswers

--- locations/storefinders/yext_answers.py
+++ locations/storefinders/yext_answers.py
@@ -9,7 +9,26 @@ from scrapy.spiders import Spider
 from locations.dict_parser import DictParser
 from locations.hours import OpeningHours
 from locations.items import Feature
+from locations.categories import Extras, apply_yes_no, PaymentMethods
+from locations.structured_data_spider import clean_facebook
 
+GOOGLE_ATTRIBUTES_MAP = {
+    "has_restroom": Extras.Toilets,
+    "has_wheelchair_accessible_restroom": Extras.TOILETS_WHEELCHAIR,
+    "has_takeout": Extras.TAKEAWAY,
+    "has_delivery": Extras.DELIVERY,
+    "accepts_reservations": Extras.RESERVATIONS, # NEEDS ADDING TO EXTRAS
+    "has_high_chairs": Extras.HIGH_CHAIR, # NEEDS ADDING TO EXTRAS
+    "pay_debit_card": PaymentMethods.DEBIT_CARDS,
+    "pay_mobile_nfc": PaymentMethods.CONTACTLESS,
+    "pay_credit_card": PaymentMethods.CREDIT_CARDS,
+}
+
+GOOGLE_WHEELCHAIR_KEYS = [
+    "has_wheelchair_accessible_restroom",
+    "has_wheelchair_accessible_entrance",
+    "has_wheelchair_accessible_seating",
+]
 
 class YextAnswersSpider(Spider):
     dataset_attributes = {"source": "api", "api": "yext"}
@@ -52,27 +71,80 @@ class YextAnswersSpider(Spider):
 
     def parse(self, response: Response, **kwargs: Any) -> Any:
         for location in response.json()["response"]["results"]:
-            item = DictParser.parse(location["data"])
-            item["branch"] = location["data"].get("geomodifier")
-            item["extras"]["ref:google"] = location["data"].get("googlePlaceId")
-            item["facebook"] = location["data"].get("facebookPageUrl")
+            location = location["data"]
+            item = DictParser.parse(location)
+            item["branch"] = location.get("geomodifier")
 
-            if website_url_dict := location["data"].get("websiteUrl"):
+            phones = []
+            for phone_type in ["localPhone", "mainPhone", "mobilePhone"]:
+                if phone := location["profile"].get(phone_type):
+                    if isinstance(phone, dict):
+                        phones.append(phone.get("number"))
+                    elif isinstance(phone, str):
+                        phones.append(phone)
+            if len(phones) > 0:
+                item["phone"] = "; ".join(phones)
+
+            if emails := location["profile"].get("emails"):
+                item["email"] = "; ".join(emails)
+
+            item["extras"]["ref:google"] = location.get("googlePlaceId")
+            item["twitter"] = location.get("twitterHandle")
+            item["extras"]["contact:instagram"] = location.get("instagramHandle")
+            if "facebookVanityUrl" in location:
+                item["facebook"] = clean_facebook(location["facebookVanityUrl"])
+            else:
+                item["facebook"] = clean_facebook(location.get("facebookPageUrl"))
+
+            if website_url_dict := location.get("websiteUrl"):
                 if website_url_dict.get("preferDisplayUrl"):
                     item["website"] = website_url_dict.get("displayUrl")
                 else:
                     item["website"] = website_url_dict.get("url")
 
-            item["opening_hours"] = self.parse_opening_hours(location)
+            if menu_url_dict := location.get("menuUrl"):
+                if menu_url_dict.get("preferDisplayUrl"):
+                    item["website"] = menu_url_dict.get("displayUrl")
+                else:
+                    item["website"] = menu_url_dict.get("url")
+
+            item["opening_hours"] = self.parse_opening_hours(location.get("hours"))
+            item["extras"]["opening_hours:delivery"] = self.parse_opening_hours(location.get("deliveryHours"))
+
+            if payment_methods := location.get("paymentOptions"):
+                payment_methods = [p.lower().replace(" ", "") for p in payment_methods]
+                apply_yes_no(PaymentMethods.AMERICAN_EXPRESS, item, "americanexpress" in payment_methods)
+                apply_yes_no(PaymentMethods.APPLE_PAY, item, "applepay" in payment_methods)
+                apply_yes_no(PaymentMethods.CASH, item, "cash" in payment_methods)
+                apply_yes_no(PaymentMethods.CHEQUE, item, "check" in payment_methods)
+                apply_yes_no(PaymentMethods.CONTACTLESS, item, "contactlesspayment" in payment_methods)
+                apply_yes_no(PaymentMethods.DINERS_CLUB, item, "dinersclub" in payment_methods)
+                apply_yes_no(PaymentMethods.DISCOVER_CARD, item, "discover" in payment_methods)
+                apply_yes_no(PaymentMethods.MASTER_CARD, item, "mastercard" in payment_methods)
+                apply_yes_no(PaymentMethods.SAMSUNG_PAY, item, "samsungpay" in payment_methods)
+                apply_yes_no(PaymentMethods.VISA, item, "visa" in payment_methods)
+
+            if google_attributes := location.get("googleAttributes"):
+                for key, attribute in GOOGLE_ATTRIBUTES_MAP:
+                    if key in google_attributes:
+                        apply_yes_no(attribute, item, google_attributes[key][0], True)
+                if "requires_reservations" in google_attributes and google_attributes["requires_reservations"][0]:
+                    item["extras"]{Extras.RESERVATIONS] = "required"
+                wheelchair_keys_present = [key for key in GOOGLE_WHEELCHAIR_KEYS if key in google_attributes]
+                if all([google_attributes[key][0] for key in wheelchair_keys_present]):
+                    apply_yes_no(Extras.WHEELCHAIR, item, True, True)
+                elif any([google_attributes[key][0] for key in wheelchair_keys_present]):
+                    item["extras"][Extras.WHEELCHAIR] = "limited"
+                else:
+                    apply_yes_no(Extras.WHEELCHAIR, item, True, True)
 
             yield from self.parse_item(location, item) or []
 
         if len(response.json()["response"]["results"]) == self.page_limit:
             yield self.make_request(response.meta["offset"] + self.page_limit)
 
-    def parse_opening_hours(self, location: dict, **kwargs: Any) -> OpeningHours | None:
+    def parse_opening_hours(self, hours: dict, **kwargs: Any) -> OpeningHours | None:
         oh = OpeningHours()
-        hours = location["data"].get("hours")
         if not hours:
             return None
         for day, rule in hours.items():
@@ -81,7 +153,7 @@ class YextAnswersSpider(Spider):
             if day == "holidayHours":
                 continue
             if rule.get("isClosed") is True:
-                continue
+                oh.set_closed(day)
             for time in rule["openIntervals"]:
                 oh.add_range(day, time["start"], time["end"])
 
